9c42c9d39de158276ef78ef060c4ffbe0c648c94
viktoriya <viktoriaseksenan@gmail.com>
2020-01-15 12:52:05 +0400

    master

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div>
+      <a href="#s">Home</a>
+      <a href="#s">New Feed</a>
+      <a href="#s">Contacts</a>
+      <a href="#s">-----</a>
+    </div>
+  );
+}
+  export default  Header;
