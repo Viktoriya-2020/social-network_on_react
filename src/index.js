988c27c7e135688ca43fa37586350e0e4391dfc7
viktoriya <viktoriaseksenan@gmail.com>
2020-02-27 21:14:42 +0400

    master

--- src/index.js
+++ src/index.js
@@ -5,17 +5,18 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from "react-router-dom";
 import store from './Redax/redux-store';
-
+import Provider from 'react-redux';
 
 let rerenderEntireTree = (state) =>{
     
        
         ReactDOM.render(
             <BrowserRouter>
-            
+                <Provider store = {store}>
                 <App store={store} 
                 dispatch = {store.dispatch.bind(store)}
                  />
+                 </Provider>
             </BrowserRouter>,
             document.getElementById('root'));
         
