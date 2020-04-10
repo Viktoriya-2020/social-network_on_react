7637368daeb9e1633651f94e8b2141a1d8aafcb5
viktoriya <viktoriaseksenan@gmail.com>
2020-04-10 20:56:59 +0400

    Updates

---
+++ precache-manifest.0f0bb7ca056953536f7278727eead3ef.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e6d6c79d076d0d7069d702d289e1b71",
+    "url": "/social-network_on_react/index.html"
+  },
+  {
+    "revision": "28e97b8fa0c3830e7e92",
+    "url": "/social-network_on_react/static/css/3.b6f19fc4.chunk.css"
+  },
+  {
+    "revision": "7e115c1c8944727e5978",
+    "url": "/social-network_on_react/static/css/4.4250e13c.chunk.css"
+  },
+  {
+    "revision": "2fcb8e6f2d1b6d66d407",
+    "url": "/social-network_on_react/static/css/main.49088c12.chunk.css"
+  },
+  {
+    "revision": "11a2f3e8fe415de8c1e2",
+    "url": "/social-network_on_react/static/js/2.7312c484.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/social-network_on_react/static/js/2.7312c484.chunk.js.LICENSE"
+  },
+  {
+    "revision": "28e97b8fa0c3830e7e92",
+    "url": "/social-network_on_react/static/js/3.d4b44459.chunk.js"
+  },
+  {
+    "revision": "7e115c1c8944727e5978",
+    "url": "/social-network_on_react/static/js/4.b3fd7907.chunk.js"
+  },
+  {
+    "revision": "2fcb8e6f2d1b6d66d407",
+    "url": "/social-network_on_react/static/js/main.013e95c3.chunk.js"
+  },
+  {
+    "revision": "c1a12f22c883ee62f4a7",
+    "url": "/social-network_on_react/static/js/runtime-main.9fa02ef0.js"
+  },
+  {
+    "revision": "c07a220fe05f09a71edbf08ee89ce83b",
+    "url": "/social-network_on_react/static/media/avatar.c07a220f.jpg"
+  },
+  {
+    "revision": "f7a27235762f789a9d2695375f45a82b",
+    "url": "/social-network_on_react/static/media/gambar-user-png-5.f7a27235.png"
+  },
+  {
+    "revision": "8b621a291a0d855aec56ac2687a520c0",
+    "url": "/social-network_on_react/static/media/progress.8b621a29.gif"
+  },
+  {
+    "revision": "6be5f38c84f68a19bf41a7a57221a1bf",
+    "url": "/social-network_on_react/static/media/wall.6be5f38c.jpg"
+  }
+]);
--- src/common/Preloader/Preloader.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Progress from '../../assets/images/progress.gif'
-
-const Preloader = () => {
-    return <div style = {{height:180}}><img src = { Progress} /></div>
-}
-export default Preloader;
